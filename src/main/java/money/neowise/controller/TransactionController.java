fd47ba2b6c1adf7152b661753bed20b298f4814a
nuuron <anuransaikia.201mt007@nitk.edu.in>
2022-04-13 19:21:04 +0530

    Controller added

---
+++ src/main/java/money/neowise/controller/TransactionController.java
@@ -0,0 +1,10 @@
+package money.neowise.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/transactions/")
+public class TransactionController {
+
+}
